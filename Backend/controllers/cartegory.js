b4705cfa3ea130c65f31a3d0139cdcac06a9ca23
samuel freeman <samuelmanueljnr@gmail.com>
2024-04-03 18:58:07 +0000

    cartegory controllers

--- Backend/controllers/cartegory.js
+++ Backend/controllers/cartegory.js
@@ -24,3 +24,53 @@ exports.regiter_cartegory = async (req, res, next) => {
      }
 };
 
+exports.getAllCartegories = async (req, res, next) => {
+     try {
+          const cartegories = await getCartegories();
+          res.status(httpstatus.OK).json({
+               cartegories,
+          });
+     } catch (error) {
+          logger.error(error);
+          next(new CustomError(500, error));
+     }
+};
+
+exports.getSingleCartegory = async (req, res, next) => {
+     try {
+          const { id } = req.param;
+          const cartegory = await getSingleCartegory(id);
+          res.status(httpstatus.OK).json({
+               cartegory,
+          });
+     } catch (error) {
+          logger.error(error);
+          next(new CustomError(500, error));
+     }
+};
+
+exports.editCartegory = async(req,res,next)=>{
+     try {
+          const { id } = req.param;
+          const data = req.body;
+          const cartegory = await editCartegory(id,data);
+          res.status(httpstatus.OK).json({
+               cartegory,
+          });
+     } catch (error) {
+          logger.error(error);
+          next(new CustomError(500, error));
+     }
+}
+exports.removeCartegory = async (req,res,next)=>{
+     try {
+          const { id } = req.param;
+          const cartegory = await removeCartegory(id);
+          res.status(httpstatus.OK).json({
+               cartegory,
+          });
+     } catch (error) {
+          logger.error(error);
+          next(new CustomError(500, error));
+     }
+}
