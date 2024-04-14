02b85daee06182f948b499d4bb8394a78477f583
lockwud <oklement3@gmail.com>
2024-04-14 05:10:12 +0000

    last try

--- Backend/helpers/cartegory.js
+++ Backend/helpers/cartegory.js
@@ -1,21 +1,21 @@
 const prisma = require("../utils/prismaUtil");
 const cloudinary = require("../utils/cloudinary");
-const addCategory = async(req,res, next) =>{
-     const data = req.body;
+const addCategory = async(req,res, data) =>{
+     
      const item  = req.file ? req.file.path : undefined;
         if (item) {
             const uploaded = await cloudinary.uploader.upload(item, {
                 folder: 'familytree/' + category.type + '/profile'
             });
             if (uploaded) {
-                data.item = uploaded.secure_url;
+                item = uploaded.secure_url;
             }
         }
+        
         const category = await prisma.category.create({
-            data:{
-               item: data.item,
-               type: data.type
-            }
+          
+              data: { item,
+               type}
         });
         res.status(httpstatus.OK).json({
           category,
