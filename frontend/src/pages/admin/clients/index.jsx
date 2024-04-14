bceb4bfb2fc9cc1bbe99623e40a9830518c13dc1
Masood Acheampong <masoodacheampong@gmail.com>
2024-04-14 12:38:34 +0100

    Completed table list and create categories, orders and recipe

--- frontend/src/pages/admin/clients/index.jsx
+++ frontend/src/pages/admin/clients/index.jsx
@@ -1,7 +1,62 @@
 import React from "react";
+import { restApi } from "../../../appSetup/api";
+import { MdDelete, MdModeEditOutline } from "react-icons/md";
 
 function Clients() {
-  return <div>Clients</div>;
+  const { data, isLoading } = restApi.useGetClientsQuery();
+  console.log(data, "data");
+  return (
+    <div className="">
+      <div className="flex items-center justify-between py-5 px-4">
+        <h1 className="text-2xl font-bold ">Clients</h1>
+      </div>
+      {/* Table */}
+      <div className="overflow-x-auto">
+        <table className="table">
+          {/* Table Head */}
+          <thead>
+            <tr>
+              <th className="text-lg">User Name</th>
+              <th className="text-lg">Email</th>
+              <th className="text-lg">Role</th>
+              <th className="text-lg">Edit</th>
+              <th className="text-lg">Delete</th>
+            </tr>
+          </thead>
+          {/* Table Body */}
+          <tbody>
+            {isLoading && <div className="loader"></div>}
+            {data?.clients?.map((client) => (
+              <tr key={client.id}>
+                {/* User name */}
+                <td>
+                  <p>{client.username}</p>
+                </td>
+                {/* Email */}
+                <td>
+                  <p>{client.email}</p>
+                </td>
+                {/* Role */}
+                <td>
+                  <p>{client.role}</p>
+                </td>
+                <th>
+                  <button className="btn btn-ghost btn-xs">
+                    <MdModeEditOutline />
+                  </button>
+                </th>
+                <th>
+                  <button className="btn btn-ghost btn-xs">
+                    <MdDelete />
+                  </button>
+                </th>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 }
 
 export default Clients;
--- frontend/src/pages/admin/orders/index.jsx
+++ frontend/src/pages/admin/orders/index.jsx
@@ -1,7 +1,72 @@
 import React from "react";
+import { IoFastFood } from "react-icons/io5";
+import { MdDelete, MdModeEditOutline } from "react-icons/md";
+import { Link } from "react-router-dom";
+import { restApi } from "../../../appSetup/api";
 
 function Orders() {
-  return <div>Orders</div>;
+  const { data, isLoading } = restApi.useGetOrderQuery();
+  console.log(data, "data");
+  return (
+    <div className="">
+      <div className="flex items-center justify-between py-5 px-4">
+        <h1 className="text-2xl font-bold ">Orders</h1>
+      </div>
+      {/* Table */}
+      <div className="overflow-x-auto">
+        <table className="table">
+          {/* Table Head */}
+          <thead>
+            <tr>
+              <th className="text-lg">View Orders</th>
+              <th className="text-lg">Quantity</th>
+              <th className="text-lg">Sub Total</th>
+              <th className="text-lg">Delivery Fee</th>
+              <th className="text-lg">Edit</th>
+              <th className="text-lg">Delete</th>
+            </tr>
+          </thead>
+          {/* Table Body */}
+          <tbody>
+            {isLoading && <div className="loader"></div>}
+            {data?.orders?.map((order) => (
+              <tr key={order.id}>
+                {/* View Orders */}
+                <td>
+                  {/*Replace with /order-recipe/${id} */}
+                  <Link to={`/order-recipe/${order.id}`}>
+                    <IoFastFood className="w-8 h-8" />
+                  </Link>
+                </td>
+                {/* Quantity */}
+                <td>
+                  <p>{order.quantity}</p>
+                </td>
+                {/* Total */}
+                <td>
+                  <p>{order.total}</p>
+                </td>
+                {/* Price */}
+                <td>
+                  <p>{order.price}</p>
+                </td>
+                <th>
+                  <button className="btn btn-ghost btn-xs">
+                    <MdModeEditOutline />
+                  </button>
+                </th>
+                <th>
+                  <button className="btn btn-ghost btn-xs">
+                    <MdDelete />
+                  </button>
+                </th>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 }
 
 export default Orders;
