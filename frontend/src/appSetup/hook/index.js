50286ab72dadf9c0752e97eb2e09c3a6e3a9e26a
Desmond Kofi Adusei <desdhi24@gmail.com>
2024-04-15 10:15:04 +0000

    chore: finishing the app

--- frontend/src/appSetup/hook/index.js
+++ frontend/src/appSetup/hook/index.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-// const BASE_URL = "http://localhost:4000/api/v1/web";
-const BASE_URL = "https://tomato-x0w7.onrender.com/api/v1/web";
+const BASE_URL = "http://localhost:4000/api/v1/web";
+// const BASE_URL = "https://tomato-x0w7.onrender.com/api/v1/web";
 
 export const restApi = createApi({
   reducerPath: "restApi",
