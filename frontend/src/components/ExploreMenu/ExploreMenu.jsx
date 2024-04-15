83c7b6e696d811227f399fa3ade4f05b38e1815e
Masood Acheampong <masoodacheampong@gmail.com>
2024-04-15 22:48:36 +0100

    Made changes to the recipe table and the recipe form

--- frontend/src/components/ExploreMenu/ExploreMenu.jsx
+++ frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -1,18 +1,18 @@
-import "./ExploreMenu.css";
-
 import { restApi } from "../../appSetup/hook";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+
 const ExploreMenu = ({ category, setCategory }) => {
   const { data, isLoading } = restApi.useGetCategoriesQuery();
   return (
-    <div className="explore-menu" id="explore-menu">
-      <h1>Explore Our Menu</h1>
-      <p className="explore-menu-text">
+    <div className="flex flex-col gap-5" id="explore-menu">
+      <h1 className="text-gray-800 font-semibold">Explore Our Menu</h1>
+      <p className="max-w-3/5 text-gray-800">
         Choose from a diverse menu featuring a delectable array of dishes. Our
         mission is to satisfy your cravings and elevate your dining experience,
         one delicious meal at a time.{" "}
       </p>
 
-      <div className="explore-menu-list">
+      <div className="flex justify-start items-center gap-7 text-center m-5 overflow-x-scroll">
         {isLoading ? (
           <div className="loader"></div>
         ) : data?.cartegories.length > 0 ? (
@@ -25,14 +25,18 @@ const ExploreMenu = ({ category, setCategory }) => {
                   );
                 }}
                 key={index}
-                className="explore-menu-list-item"
+                className="cursor-pointer"
               >
-                <img
-                  className={category === item.type ? "active" : ""}
-                  src={item.item}
+                <LazyLoadImage
+                  className={`w-16 h-auto min-w-20 rounded-full transition-all duration-200 ${
+                    category === item.type ? "border-4 border-tomato p-1" : ""
+                  }`}
+                  src={item.image}
                   alt=""
                 />
-                <p>{item.type}</p>
+                <p className="mt-2 text-gray-400 text-base cursor-pointer">
+                  {item.type}
+                </p>
               </div>
             );
           })
@@ -42,7 +46,7 @@ const ExploreMenu = ({ category, setCategory }) => {
           </div>
         )}
       </div>
-      <hr />
+      <hr className="my-2 h-0.5 bg-gray-300 border-none" />
     </div>
   );
 };
